5ee7dfc7bf9fb5f9e686195e25e1d1b98991ab17
Marshal Wu <marshal.wu@gmail.com>
2022-04-19 12:04:11 +0800

    fix build error

--- cp-nocobase-packages.sh
+++ cp-nocobase-packages.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
 ###
- # @Author: pangff
- # @Date: 2022-04-13 15:42:47
- # @LastEditTime: 2022-04-19 09:49:58
- # @LastEditors: pangff
- # @Description: 复制脚本
- # @FilePath: /noco-next-demo/cp-nocobase-packages.sh
- # stay hungry,stay foolish
-### 
+# @Author: pangff
+# @Date: 2022-04-13 15:42:47
+# @LastEditTime: 2022-04-19 09:49:58
+# @LastEditors: pangff
+# @Description: 复制脚本
+# @FilePath: /noco-next-demo/cp-nocobase-packages.sh
+# stay hungry,stay foolish
+###
 
 nocobase_dir=$1
 
 packages=(
-    "core/utils"
-    "core/server"
-    "core/database"
-    "core/resourcer"
-    "core/actions"
-    "core/acl"
-    "plugins/acl"
-    "plugins/users"
-    "plugins/error-handler"
+    "utils"
+    "server"
+    "database"
+    "resourcer"
+    "actions"
+    "acl"
 )
 
 rm -rf node_modules/@nocobase
@@ -28,5 +25,15 @@ rm -rf node_modules/@nocobase
 mkdir node_modules/@nocobase
 
 for package in ${packages[@]}; do
-    cp -r $nocobase_dir/packages/$package node_modules/@nocobase
+    cp -r $nocobase_dir/packages/core/$package node_modules/@nocobase
+done
+
+packages=(
+    "acl"
+    "users"
+    "error-handler"
+)
+
+for package in ${packages[@]}; do
+    cp -r $nocobase_dir/packages/plugins/$package node_modules/@nocobase/plugin-$package/
 done
